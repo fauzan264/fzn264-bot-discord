2951dec06ac7d96276999116cca18f61b99b0e34
Fauzan <fauzan.c4@gmail.com>
2025-05-04 23:57:09 +0700

    project first commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+from discord import Intents
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+# create object intents
+intents = Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot aktif sebagai {bot.user.name}")
+
+@bot.command(name="ping")
+async def hello(ctx):
+    await ctx.send("Pong!")
+
+@bot.command(name="landofdawn")
+async def mobile_legends_team(ctx):
+
+    users = [
+        ""
+    ]
+
+    mentions = " ".join([f"<@{user}>" for user in users])
+    await ctx.send(f"{mentions}, ayo mabar!")
+
+
+@bot.command(name="indonesia")
+async def indonesian(ctx):
+    await ctx.send("Indonesia besar, Indonesia kuat, Indonesia cerah")
+
+# async def main():
+#     # load all file command from folder "cmd"
+#     for filename in os.listdir("./cmd"):
+#         if filename.endswith(".py") and filename != "__init__.py":
+#             try:
+#                 await bot.load_extension(f"cmd.{filename[:-3]}")
+#                 print(f"Memuat extension {filename[:-3]}")
+#             except Exception as e:
+#                 print(f"Gagal memuat extension {filename[:-3]}: {e}")
+
+bot.run(TOKEN)
+
