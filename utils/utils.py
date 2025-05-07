47f8f555336516cf00121c95c620b0bc7f7249c4
Fauzan <fauzan.c4@gmail.com>
2025-05-08 03:01:44 +0700

    create function send_message to channel

--- utils/utils.py
+++ utils/utils.py
@@ -8,3 +8,10 @@ def load_file(csv_file):
         csv_reader = csv.DictReader(file)
         rows = [row for row in csv_reader]
         return rows
+    
+async def send_message(bot, channel_id, message):
+    try:
+        channel = await bot.fetch_channel(channel_id)
+        await channel.send(message)
+    except Exception as e:
+        print(f"Error: {e}, channel {channel_id} not found!")
