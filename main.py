c1b6c31d4b3d14d53bf5b9f4cc5a6f793a90ce87
Fauzan <fauzan.c4@gmail.com>
2025-05-08 06:21:16 +0700

    refactor code

--- main.py
+++ main.py
@@ -3,31 +3,22 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import os
 from dotenv import load_dotenv
 from utils import utils
+from utils.logger import setup_logger
+from scheduler.jobs import setup_scheduler
 from discord import Intents
 import random
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-# CHANNEL_ID
-GENERAL_CHANNEL = os.getenv("GENERAL_CHANNEL")
-MLBB_CHANNEL = os.getenv("MLBB_CHANNEL")
-
-# SCHEDULER
-TODAY_WORDS_SCHEDULE_HOURS = os.getenv("TODAY_WORDS_SCHEDULE_HOURS")
-TODAY_WORDS_SCHEDULE_MINUTES = os.getenv("TODAY_WORDS_SCHEDULE_MINUTES")
-
-MABAR_SCHEDULE_HOURS = os.getenv("MABAR_SCHEDULE_HOURS")
-MABAR_SCHEDULE_MINUTES = os.getenv("MABAR_SCHEDULE_MINUTES")
+# Logger, Scheduler, Bot
+logger = setup_logger()
+scheduler = AsyncIOScheduler()
 
 # create object intents
 intents = Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
-
-@bot.event
-async def on_ready():
-    print(f"Active bot as {bot.user.name}")
 
 rows = utils.load_file("cmd.csv")
 
@@ -69,43 +60,10 @@ for row in rows:
         @bot.command(name=cmd)
         async def _(ctx, response=response): await ctx.send(response)
 
-    # CREATE SCHEDULER
-    scheduler = AsyncIOScheduler()
-
-    @bot.event
-    async def on_ready():
-        # KATA-KATA HARI INI
-        channel_id = GENERAL_CHANNEL
-        available_quotes = [quote for quote in quotes if quote not in used_quotes]
-        if not available_quotes:
-            used_quotes.clear()
-            available_quotes = quotes
-        message = random.choice(quotes)
-        used_quotes.append(message)
-        
-        send_message = f"kata-kata hari ini: {message['quotes']}"
-        
-        scheduler.add_job(
-            utils.send_message,
-            'cron',
-            hour=TODAY_WORDS_SCHEDULE_HOURS,
-            minute=TODAY_WORDS_SCHEDULE_MINUTES,
-            timezone="Asia/Jakarta",
-            args=[bot, channel_id, send_message]
-        )
-        
-        # MABAR
-        channel_id = MLBB_CHANNEL
-        message = "Apakah tidak ada per-mabaran duniawi hari ini?"
-        
-        scheduler.add_job(
-            utils.send_message,
-            'cron',
-            hour=MABAR_SCHEDULE_HOURS,
-            minute=MABAR_SCHEDULE_MINUTES,
-            timezone="Asia/Jakarta",
-            args=[bot, channel_id, message]
-        )
-        scheduler.start()
+@bot.event
+async def on_ready():
+    logger.info(f"Active bot as {bot.user.name}")
+    setup_scheduler(bot, scheduler, logger)
+    scheduler.start()
 
 bot.run(TOKEN)
