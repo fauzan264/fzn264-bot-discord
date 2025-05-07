b69c41c57153e18f2d1039f2839665a4a26178ab
Fauzan <fauzan.c4@gmail.com>
2025-05-08 06:20:34 +0700

    update logger

--- utils/logger.py
+++ utils/logger.py
@@ -1,9 +1,22 @@
 import logging
+from colorlog import ColoredFormatter
 
 def setup_logger():
     logger = logging.getLogger("discord_bot")
     logger.setLevel(logging.INFO)
     handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter("[%(asctime)s] %(levelname)s: %(message)s"))
+    formatter = ColoredFormatter(
+        "[%(asctime)s] %(log_color)s%(levelname)s%(reset)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        log_colors={
+            "DEBUG": "cyan",
+            "INFO": "blue",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "bold_red"
+        },
+        reset=True
+    )
+    handler.setFormatter(formatter)
     logger.addHandler(handler)
     return logger
